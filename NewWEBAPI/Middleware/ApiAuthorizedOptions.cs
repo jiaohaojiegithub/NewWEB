e883dedc30baf402c66f97415b4edc1f5d3bb50a
豪杰 焦 <2351592225@qq.com>
2018-07-17 22:38:07 +0800

    API中间件Key

---
+++ NewWEBAPI/Middleware/ApiAuthorizedOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewWEBAPI.Middleware
+{
+    public class ApiAuthorizedOptions
+    {
+        public string Name { get; set; }
+        /// <summary>
+        /// EncryptKey ，用于对我们的请求参数进行签名
+        /// </summary>
+        public string EncryptKey { get; set; }
+        /// <summary>
+        /// ExpiredSecond ，用于检验我们的请求是否超时
+        /// </summary>
+        public int ExpiredSecond { get; set; }
+    }
+}
